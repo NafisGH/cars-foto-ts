29a9a3d5629469a9db19c47f44412c373a13bf2b
Nafis Garifullin <garifullinn@gmail.com>
2023-10-09 19:20:32 +0500

    second comit

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./providers/AppRouter";
-import 'app/styles/index.scss';
-// import Rating from "components/Rating/Rating";
-// import Accordion from "components/Accordion/Accordion";
-
+import "app/styles/index.scss";
+import React from "react";
 
 function App() {
   return (
     <BrowserRouter>
-        <AppRouter/>
+      <AppRouter />
     </BrowserRouter>
-    
   );
 }
 
